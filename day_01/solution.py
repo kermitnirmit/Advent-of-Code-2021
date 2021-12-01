ec932147f5c579efe79745e67d35eb611fe11428
Nirmit Shah <nirmit@squareup.com>
2021-12-01 15:04:59 -0500

    booleans get evaluated!!!

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,3 +1,3 @@
 f = [int(x) for x in open("input.txt").read().strip().split("\n")]
-print(sum(int(a > b) for a, b in zip(f[1:], f[:-1])))
-print(sum(int(a > b) for a, b in zip(f[3:], f[:-3])))
+print(sum(a > b for a, b in zip(f[1:], f[:-1])))
+print(sum(a > b for a, b in zip(f[3:], f[:-3])))
