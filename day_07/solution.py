977d04b4cef64651f88868ea6ef1fb537cc0c7a2
Nirmit Shah <nirmit@squareup.com>
2021-12-07 00:07:30 -0500

    day 7 done

---
+++ day_07/solution.py
@@ -0,0 +1,18 @@
+f = [int(x) for x in open("input.txt").read().strip().split(",")]
+
+def gen_number(n):
+    return int(n*(n+1)/2)
+a1, a2 = [], []
+for i in range(len(f)):
+    rsum = 0
+    rsum2 = 0
+    for a in f:
+        rsum += abs(a - i)
+        rsum2 += gen_number(abs(a - i))
+    a1.append(rsum)
+    a2.append(rsum2)
+
+print(min(a1))
+print(min(a2))
+
+
