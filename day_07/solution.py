32c24686187a11e9b4f5fb05e305238ca1ff15b4
Nirmit Shah <nirmit@squareup.com>
2021-12-07 13:16:17 -0500

    fixed everything

--- day_07/solution.py
+++ day_07/solution.py
@@ -1,7 +1,15 @@
+import statistics
 f = [int(x) for x in open("input.txt").read().strip().split(",")]
 
-def gen_number(n):
-    return int(n*(n+1)/2)
+# Turns out median and mean do the trick.
+p1 = int(statistics.median(f))
+p2 = int(statistics.mean(f))
 
-print(min([[sum(abs(a-i) for a in f)] for i in range(len(f))]))
-print(min([[sum(gen_number(abs(a - i)) for a in f)] for i in range(len(f))]))
+def gen_number(n):
+    return n * (n+1) // 2
+
+print(sum(abs(a-p1) for a in f))
+print(sum(gen_number(abs(a-p2)) for a in f))
+
+print(min([[sum(abs(a-i) for a in f)] for i in range(max(f))]))
+print(min([[sum(gen_number(abs(a - i)) for a in f)] for i in range(max(f))]))
