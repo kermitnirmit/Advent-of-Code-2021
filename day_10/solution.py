26a216a9396e442daad33616b2e61fce54c07cdd
Nirmit Shah <nirmit@squareup.com>
2021-12-10 00:57:52 -0500

    cleaned up

--- day_10/solution.py
+++ day_10/solution.py
@@ -1,9 +1,6 @@
 import statistics
 f = [x for x in open("input.txt").read().strip().split("\n")]
 
-c = 0
-p = 1
-
 p1_score = {
     ")": 3,
     "]": 57,
@@ -17,54 +14,36 @@ open_to_close = {
     "(": ")",
     "<": ">"
 }
-
-a = []
-badIndexes = []
-for i, line in enumerate(f):
-    opens = []
-    for char in line:
-        if char in p1_score.keys():
-            if open_to_close[opens[-1]] != char:
-                a.append(p1_score[char])
-                opens.pop(-1)
-                badIndexes.append(i)
-                break
-            else:
-                opens.pop(-1)
-        else:
-            opens.append(char)
-
-
-goodLines = []
-for i, line in enumerate(f):
-    if i not in badIndexes:
-        goodLines.append(line)
 part_2_score = {
     ")": 1,
     "]": 2,
     "}": 3,
     ">": 4
 }
+
+a = []
 p2_scores = []
-for line in goodLines:
+for i, line in enumerate(f):
     opens = []
+    corrupt = False
     for char in line:
         if char in p1_score.keys():
-            if open_to_close[opens[-1]] != char:
+            if open_to_close[opens[-1]] != char and not corrupt:
                 a.append(p1_score[char])
                 opens.pop(-1)
-                badIndexes.append(i)
-                break
+                corrupt = True
             else:
                 opens.pop(-1)
         else:
             opens.append(char)
-    rScore = 0
-    while len(opens) != 0:
-        blob = opens.pop(-1)
-        rScore *= 5
-        rScore += part_2_score[open_to_close[blob]]
-    p2_scores.append(rScore)
+    if not corrupt:
+        rScore = 0
+        while len(opens) != 0:
+            blob = opens.pop(-1)
+            rScore *= 5
+            rScore += part_2_score[open_to_close[blob]]
+        p2_scores.append(rScore)
+
 
 print(sum(a))
 print(statistics.median(p2_scores))
