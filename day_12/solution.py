722efec5802ee187ebdd94b716f59974d0b8eac9
Nirmit Shah <nirmit@squareup.com>
2021-12-12 01:31:22 -0500

    that was brutal

---
+++ day_12/solution.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+
+f = [x for x in open("input.txt").read().strip().split("\n")]
+vertexes = []
+
+class Graph:
+
+    def __init__(self, V):
+        self.V = V
+        self.adj = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.adj[u].append(v)
+        self.adj[v].append(u)
+
+    def explore(self, n, visited, twice=False):
+        if n == "end":
+            return 1
+        c = 0
+        for node in self.adj[n]:
+            if node.islower():
+                if node not in visited:
+                    c += self.explore(node, visited.union({node}), twice)
+                elif not twice and node not in "startend":
+                    # found something that happened twice already so don't go again
+                    c += self.explore(node, visited.union({node}), True)
+            else:
+                c += self.explore(node, visited, twice)
+        return c
+
+
+for line in f:
+    left, right = line.split("-")
+    vertexes.append(left)
+    vertexes.append(right)
+
+g = Graph(vertexes)
+for line in f:
+    l, r = line.split("-")
+    g.addEdge(l,r)
+
+print(g.explore("start", {"start"}, True))
+print(g.explore("start", {"start"}, False))
