57bb11052f1844119942e01aee5fd433cb6c469b
Nirmit Shah <nirmit@squareup.com>
2021-12-14 01:39:26 -0500

    explanations for brian

--- day_14/solution.py
+++ day_14/solution.py
@@ -1,31 +1,31 @@
 from collections import defaultdict, Counter
 f = [x for x in open("input.txt").read().strip().split("\n")]
 
-start = f[0]
-parts = f[2:]
-d = defaultdict(str)
+start = f[0]  # first line is the start str
+parts = f[2:] # everything after the 2nd line is a mapping
+d = defaultdict(str) # instantiate a dictionary that maps something to a string (kinda not necessary)
 
 for part in parts:
     left, right = part.split(" -> ")
-    d[left] = right
+    d[left] = right # left double maps to right single
 
-d1 = defaultdict(int)
-for a,b in zip(start[:-1], start[1:]):
+d1 = defaultdict(int) #instantiate dictionary that maps something to an int
+for a,b in zip(start[:-1], start[1:]): # this iterates over the letter pairs in the initial string
     q = a+b
-    d1[q] += 1
+    d1[q] += 1 # counts how many of each pair
 
 for i in range(40):
-    d2 = defaultdict(int)
-    for k,v in d1.items():
-        d2[k[0] + d[k]] += v
-        d2[d[k] + k[1]] += v
-    d1 = d2
+    d2 = defaultdict(int) #because the changes are instantaneous - need to keep a new and old dictionary
+    for k,v in d1.items(): # k is a letter pair thats in the last generation string, v is how many times that pair occurred
+        d2[k[0] + d[k]] += v  # if k = AB and AB maps to R, then that generates an AR and RB. How many of those? v of them. this line takes care of AR
+        d2[d[k] + k[1]] += v # and this line takes care of RB
+    d1 = d2 # generation done, so store these new numbers for the next iteration
     if i in [9, 39]:
-        q = Counter()
-        for k,v in d1.items():
-            q[k[0]] += v
-        q[start[-1]] += 1 # Need to add the last character
-        print(max(q.values()) - min(q.values()))
+        q = Counter() #same as defaultdict(int)
+        for k,v in d1.items(): # same as line 19, but each letter pair overlaps, so i need to only count the first one
+            q[k[0]] += v # k[0] is the first letter in the pair, count how many times that character appeared
+        q[start[-1]] += 1 # Need to add the last character because it's never the first character in a pair.
+        print(max(q.values()) - min(q.values())) # most common - least common.
 
 
 
