d4180338f8d1b65432130ec6ee8f2f2d88fd66f2
Nirmit Shah <nirmit@squareup.com>
2021-12-15 01:04:06 -0500

    done with day 15

---
+++ day_15/solution.py
@@ -0,0 +1,36 @@
+import numpy as np
+from heapq import heappush, heappop
+f = [x for x in open("input.txt").read().strip().split("\n")]
+
+def solve(targetx, targety):
+    board = []
+    for line in f:
+        a = list(line)
+        a = [int(x) for x in a]
+        board.append(a)
+    board = np.array(board)
+
+    heap = [(0,0,0)]
+    seen = {(0,0)}
+    dirs = [(-1,0), (1, 0), (0, 1), (0, -1)]
+
+    while heap:
+        d, x, y = heappop(heap)
+        if x == targetx and y == targety:
+            return d
+        for dx, dy in dirs:
+            nx = dx + x
+            ny = dy + y
+            if 0 <= nx <= targetx and 0 <= ny <= targety:
+                i_x = nx // len(board)
+                i_xoffset = nx % len(board)
+                i_y = ny // len(board[0])
+                i_yoffset = ny % len(board[0])
+                nd = (board[i_xoffset][i_yoffset] + i_x + i_y)
+                nd = nd % 10 + nd // 10 + d
+                if (nx, ny) not in seen:
+                    heappush(heap, (nd, nx, ny))
+                    seen.add((nx, ny))
+
+print(solve(99,99))
+print(solve(499,499))
