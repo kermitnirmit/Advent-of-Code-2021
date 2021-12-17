b85f45ec7c1aab9c78ce16c647c637588fe328a9
Nirmit Shah <nirmit@squareup.com>
2021-12-17 01:24:23 -0500

    day 17

--- day_17/solution.py
+++ day_17/solution.py
@@ -1,4 +1,3 @@
-from tqdm import trange
 f = open("input.txt").read().strip().split("\n")
 """
 The probe's x position increases by its x velocity.
@@ -6,14 +5,15 @@ The probe's y position increases by its y velocity.
 Due to drag, the probe's x velocity changes by 1 toward the value 0; that is, it decreases by 1 if it is greater than 0, increases by 1 if it is less than 0, or does not change if it is already 0.
 Due to gravity, the probe's y velocity decreases by 1.
 """
+xmin, xmax, ymin, ymax = 94, 151, -156, -103
 max_ys = []
-for q in trange(1, 1000):
-    for w in range(-156, 1000):
-        x,y = 0,0
+for q in range(1, xmax + 1):
+    for w in range(ymin, ymin * -1):
+        x,y = 0, 0
         dx = q
         dy = w
-        y_pos = []
-        for _ in range(2000):
+        y_pos = -100000
+        while True:
             x += dx
             y += dy
             dy -= 1
@@ -21,17 +21,14 @@ for q in trange(1, 1000):
                 dx -= 1
             elif dx < 0:
                 dx += 1
-            if dx > 0 and x > 151:
+            if (dx > 0 and x > xmax) \
+                    or (dx < 0 and x < xmin) \
+                    or (dy < 0 and y < ymin) \
+                    or (dx == 0 and not (xmin <= x <= xmax)):
                 break
-            if dx < 0 and x < 94:
-                break
-            if dy < 0 and y < -156:
-                break
-            if dx == 0 and not (94 <= x <= 151):
-                break
-            y_pos.append(y)
-            if 94 <= x <= 151 and -156 <= y <= -103:
-                max_ys.append(max(y_pos))
+            y_pos = max(y, y_pos)
+            if xmin <= x <= xmax and ymin <= y <= ymax:
+                max_ys.append(y_pos)
                 break
 print(max(max_ys))
 print(len(max_ys))
